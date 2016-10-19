cb39ed754e1f6f3d37585b60f4ff175e528c995b
cassthecoder <cassandra@matosmendez.com>
2016-10-18 20:15:17 -0700

    Initialize repo and first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homestate = 'Arizona'
+  	@countries = ['United States','Puerto Rico', 'Germany', 'Thailand']
+  	@pictures = ['united_states.jpg', 'puerto_rico.jpg', 'germany.jpg', 'thailand.jpg']
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size]
+  end
+end
