e0c5a623f41885cca4cdea9bf854e5970061962d
cassthecoder <cassandra@matosmendez.com>
2016-10-19 11:04:34 -0700

    background img and updated stylesheets

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
+		@united_states = {'Capital City' => 'Washington, D.C.', 'Favorite City' => 'San Fran, CA', 'Favorite State' => 'Califas', 'Flag Colors'=>['red', 'white', 'blue']}
 	  	@homestate = 'Arizona'
 	  	@countries = ['United States','Puerto Rico', 'Germany', 'Thailand']
 	  	@pictures = ['united_states.jpg', 'puerto_rico.jpg', 'germany.jpg', 'thailand.jpg']
